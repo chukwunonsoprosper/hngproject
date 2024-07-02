119ee5b29912a6f91490682abdbd2a1a5a2bced0
Chukwunonso prosper <114858924+chukwunonsoprosper@users.noreply.github.com>
2024-07-02 13:21:57 +0100

    sned

--- main.js
+++ main.js
@@ -26,23 +26,42 @@ async function load() {
         let country = res['city'];
 
 
-        console.log(ip + ' ' + country)
+        async function getWeather() {
+          const city = 'lagos';
+          const apiKey = '4db2a2adbdd4dc4ceff76ab4135f9bd2';
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+          try {
+              const response = await fetch(url);
+
+              const data = await response.json();
+              const temp = data.main.temp;
+
+              async function sendover() {
+                let url1 =  `https://corsproxy.io/?https://alchemistschnews.000webhostapp.com/api/trainapi?ip=${ip}&country=${country}?weather=${temp}`;
+                try {
+                    let send = await fetch(url1);
+                    let okay = await send.text();
+                    console.log(okay);
+    
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+    
+            sendover()
+              console.log(temp)
+
+          } catch (error) {
+              console.log(error)
+          }
+      }
+
+getWeather()
 
         //sendtobackend
 
-        async function sendover() {
-            let url1 =  `https://corsproxy.io/?https://alchemistschnews.000webhostapp.com/api/trainapi?ip=${ip}&country=${country}`;
-            try {
-                let send = await fetch(url1);
-                let okay = await send.text();
-                console.log(okay);
 
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        sendover()
     } catch (error) {
         console.log(error)
     }
